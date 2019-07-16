5a8ab1a7a53439847f5b671ead77f3c4b9cea238
Lean <gustavolama20@gmail.com>
2019-07-16 18:03:56 +0300

    removed dependencies.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const validateIP = require('validate-ip-node');
+const request = require('request');
+const whois = require('whois');
+const ping = require('ping');
+const traceroute = require('nodejs-traceroute');
+
 const fs = require('fs');
 const config = require('./config.js');
 
